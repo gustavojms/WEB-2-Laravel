ec3a22503b8b4cbf4c4a91b95e5443a7b275b893
gustavojms <gustavo.joset0@hotmail.com>
2021-09-30 17:55:43 -0300

    Implementação do starter kit breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/index', HomeController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
