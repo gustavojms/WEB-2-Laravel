32d813ee6f53c535b85e9e8643350926631c8cd3
gustavojms <gustavo.joset0@hotmail.com>
2021-10-16 23:40:15 -0300

    Criação de novas rotas e ajustes no model e layout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ObraController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,27 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/home', function () {
+   return view('home');
+})->middleware(['auth'])
+  ->name('home');
+
+Route::get('/catalogo', [ObraController::class, 'index'])
+    ->name('catalogo');
+Route::delete('/catalogo/{obra}', [ObraController::class, 'destroy'])
+    ->name('remover-obra');
+Route::get('/obra/adicionar', [ObraController::class, 'create'])
+    ->middleware(['auth'])
+    ->name('adicionar-obra');
+Route::post('/obra/adicionar', [ObraController::class, 'store'])
+    ->middleware(['auth'])
+    ->name('adicionar-obra');
+Route::get('/obra/{obra}/edit', [ObraController::class, 'edit'])
+    ->middleware(['auth'])
+    ->name('editar-obra');
+Route::put('/obra/{obra}', [ObraController::class, 'update'])
+    ->middleware(['auth'])
+    ->name('editar-obra');
 
 require __DIR__.'/auth.php';
